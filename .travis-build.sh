789a17e205e3067e39ac021a621cb052ceef8c12
Hekar Khani <hekark@gmail.com>
2019-02-06 14:56:13 -0500

    Added travis-ci file for Linux builds

---
+++ .travis-build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y gcc-multilib g++-multilib build-essential rsync
+
+cd "$BUILDPATH/mp/src"
+./createallprojects
+make -f everything.mak
+rsync --progress -v ../game/mod_hl2mp/bin/* "$BUILDPATH"
