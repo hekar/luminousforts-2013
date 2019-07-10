8d7e25133be107bbef8f10be31baad74c2f2f5cb
Hekar Khani <hekark@gmail.com>
2019-07-10 18:34:54 -0400

    Remove TravisCI files

--- .travis-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sudo apt-get update
-sudo apt-get install -y gcc-multilib g++-multilib build-essential rsync
-
-cd "$BUILDPATH/mp/src"
-./createallprojects
-make -f everything.mak
-rsync --progress -v ../game/mod_hl2mp/bin/* "$BUILDPATH"
